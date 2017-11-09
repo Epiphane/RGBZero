8e06d7c0dc247ebc8165838610a3f2650a2740c4
Epiphane <exyphnos@gmail.com>
2017-11-09 00:39:42 -0800

    Initial commit

---
+++ Assets/Scripts/DumbInputScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DumbInputScript : MonoBehaviour {
+
+    public float turnSpeed = 10;
+    public float speed = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        transform.Rotate(Vector3.up * Time.deltaTime * horizontal * turnSpeed);
+        transform.position = transform.position + Time.deltaTime * transform.forward * vertical * speed;
+	}
+}
